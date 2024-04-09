6f231f2a19f04b82017d5ed8adacbaa06ffce28b
dorzhevsky <d.orzhevsky@gardatech.ru>
2024-04-09 15:50:16 +0300

    CleanUp

--- Modules.Users.Adapter.Api.Tests/Setup.cs
+++ Modules.Users.Adapter.Api.Tests/Setup.cs
@@ -11,23 +11,19 @@ namespace Modules.Users.Adapter.Api.Tests
     {
         public static ServiceProvider Init(Action<IServiceCollection> afterAction)
         {
-            IServiceCollection services = new ServiceCollection();
-
             IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-
-            services.AddSingleton(config);
-            services.AddSingleton<ILoggingManager, LoggingManager>();
-
-            services.AddModularizer(config,
-                typeof(Pg.Modules).Assembly,
-                typeof(Svc.Modules).Assembly,
-                typeof(Handlers.Modularize.Modules).Assembly
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(config)
+                    .AddSingleton<ILoggingManager, LoggingManager>()
+                    .AddModularizer(config,
+                        typeof(Pg.Modules).Assembly,
+                        typeof(Svc.Modules).Assembly,
+                        typeof(Handlers.Modularize.Modules).Assembly
             );
 
             afterAction(services);
 
-            var serviceProvider = services.BuildServiceProvider();
-            return serviceProvider;
+            return services.BuildServiceProvider();
         }
     }
 }
